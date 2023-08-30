c5eefa7d09854f8b7773183f0a136f590213a2d4
RyanPintoPassos <119443198+RyanPintoPassos@users.noreply.github.com>
2023-08-30 14:20:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 song = "";
+
 leftWristX = 0;
 leftWristY = 0;
+
 rightWristX = 0;
 rightWristY = 0;
 
@@ -26,6 +28,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrsit = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
@@ -43,6 +48,19 @@ function modelLoaded() {
 function draw() 
 {
     image(video, 0, 0, 600, 500);
+
+    fill("FF0000");
+    stroke("FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY,20);
+        InNumberLeftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberLeftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play() 
